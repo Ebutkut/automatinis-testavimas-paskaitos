2ebdaa61b45e93dd37f62699fb7d3469dff5383b
Ebutkut <erika.butkut@gmail.com>
2023-02-24 12:48:14 +0200

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Example of how a unit test could be done in a stupid way
+            int sum = Functions.Sum_Int(5, 5);
+            if (sum == 10)
+            {
+                Console.WriteLine("Works");
+            }
+            else
+            {
+                Console.WriteLine("Bug!!!");
+            }
+        }
+    }
+}
