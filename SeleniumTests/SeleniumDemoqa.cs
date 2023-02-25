8e8153aa14efe4ee419f1d989399cb6afbd5f063
Ebutkut <erika.butkut@gmail.com>
2023-02-25 20:21:36 +0200

    Add  test code fot Selenium Demoqa website

---
+++ SeleniumTests/SeleniumDemoqa.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace SeleniumTests
+{
+    public class SeleniumDemoqa
+    {
+        [TestCase("ErikaB", "test", "Kaunas", "Lithuania")]
+        public void TextBoxFormFourInputField(string valueFullName, string valueEmail, string valueCurrentAddress, string valuePermanentAddress, string expectedResult)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://demoqa.com/text-box";
+
+            IWebElement inputFullName = driver.FindElement(By.XPath("//*[@id=\"userName\"]"));
+            IWebElement inputEmail = driver.FindElement(By.XPath("//*[@id=\"userEmail\"]"));
+            IWebElement inputCurrentAddress = driver.FindElement(By.XPath("//*[@id=\"currentAddress\"]"));
+            IWebElement inputPermanentAddress = driver.FindElement(By.XPath("//*[@id=\"permanentAddress\"]"));
+            IWebElement buttonSubmit = driver.FindElement(By.XPath("//*[@id=\"submit\"]"));
+            IWebElement output = driver.FindElement(By.XPath("//*[@id=\"output\"]/div"));
+
+            inputFullName.SendKeys(valueFullName);
+            inputEmail.SendKeys(valueEmail);
+            inputCurrentAddress.SendKeys(valueCurrentAddress);
+            inputPermanentAddress.SendKeys(valuePermanentAddress);
+            buttonSubmit.Click();
+            string actualResult = output.Text;
+
+            driver.Quit();
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
+
