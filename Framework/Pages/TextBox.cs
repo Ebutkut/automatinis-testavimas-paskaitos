beb92ca32daa385c87d27270810fe7c542914745
Ebutkut <erika.butkut@gmail.com>
2023-03-01 15:06:17 +0200

    Add tests codes with POM for web Demoqa

---
+++ Framework/Pages/TextBox.cs
@@ -0,0 +1,68 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools.V108.LayerTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Pages
+{
+    public class TextBox
+    {
+        public static void ClickSubmit()
+        {
+         
+            string locator = "//*[@id='submit']";
+            Common.ClickElement(locator);
+        }
+
+        public static string CurrentAddress()
+        {
+          string locator = "//*[@id='output']//*[@id='currentAddress']";
+          return Common.GetElementText(locator);
+        }
+
+        public static void EnterCurrentAddress(string valueCurrentAddress)
+        {
+            string locator = "//*[@id='currentAddress']";
+            Common.SendKeys(locator, valueCurrentAddress);
+        }
+
+        public static void EnterEmail(string valueEmail)
+        {
+            string locator = "//*[@id='userEmail']";
+            Common.SendKeys(locator, valueEmail);
+        }
+
+        public static void EnterFullName(string valueFullName)
+        {
+            string locator = "//*[@id='userName']";
+            Common.SendKeys(locator, valueFullName);
+        }
+
+        public static void EnterPermanentAddress(string valuePermanentAddress)
+        {
+            string locator = "//*[@id='permanentAddress']";
+            Common.SendKeys(locator, valuePermanentAddress);
+        }
+
+        public static string GetEmail()
+        {          
+            string locator = "//*[@id='output']//*[@id='email']";
+            return Common.GetElementText(locator);
+        }
+
+        public static string GetFullName()
+        {
+            string locator = "//*[@id='output']//*[@id='name']";
+            return Common.GetElementText(locator);
+        }
+
+        public static string PermanentAddress()
+        {
+            string locator = "//*[@id='output']//*[@id='permanentAddress']";
+            return Common.GetElementText(locator);
+        }
+    }
+}
---
+++ Tests/TextBoxTests.cs
@@ -0,0 +1,43 @@
+﻿using Framework;
+using Framework.Pages;
+using NUnit.Framework;
+
+namespace Tests
+{
+    internal class TextBoxTests
+    {
+        [Test]
+        public void FormWithValidData()
+        {
+
+            Driver.OpenUrl("https://demoqa.com/text-box");
+            
+
+            string valueFullName = "Tester";
+            string valueEmail = "test@tester.com";
+            string valueCurrentAddress = "Test Address";
+            string valuePermanentAddress = "Test Address 2";
+
+            TextBox.EnterFullName(valueFullName);
+            TextBox.EnterEmail(valueEmail);
+            TextBox.EnterCurrentAddress(valueCurrentAddress);
+            TextBox.EnterPermanentAddress(valuePermanentAddress);
+            TextBox.ClickSubmit();
+
+            //driver.ExecuteJavaScript("window.scrollBy(0, 200)")????????????;
+            
+            string actualResult1 = TextBox.GetFullName();
+            string actualResult2 = TextBox.GetEmail();
+            string actualResult3 = TextBox.CurrentAddress();
+            string actualResult4 = TextBox.PermanentAddress();
+
+            Assert.AreEqual($"Name:{valueFullName}", TextBox.GetFullName());
+            Assert.AreEqual($"Email:{valueEmail}", TextBox.GetEmail());
+            Assert.AreEqual($"Current Address :{valueCurrentAddress}", TextBox.CurrentAddress());
+            Assert.AreEqual($"Permananet Address :{valuePermanentAddress}", TextBox.PermanentAddress());
+
+            Driver.QuitDriver();
+
+        }
+     }
+}
